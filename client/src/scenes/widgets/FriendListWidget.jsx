44a88f7ecf3677e79a86d5f6e21d1680b82e46ce
Sahil Sagvekar <sahilsagvekar230@gmail.com>
2024-06-10 14:29:35 +0530

    Update FriendListWidget.jsx

--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -13,7 +13,7 @@ const FriendListWidget = ({ userId }) => {
 
   const getFriends = async () => {
     const response = await fetch(
-      `https://mern-social-media-website-git-main-sahilsagvekars-projects.vercel.app/users/${userId}/friends`,
+      `https://social-media-website-5.onrender.com/users/${userId}/friends`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
