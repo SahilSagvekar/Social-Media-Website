edb34422e5554e27064d1a76ce2e7c660adc9a6a
Sahil Sagvekar <sahilsagvekar230@gmail.com>
2024-06-10 14:31:54 +0530

    Update MyPostWidget.jsx

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -46,7 +46,7 @@ const MyPostWidget = ({ picturePath }) => {
       formData.append("picturePath", image.name);
     }
 
-    const response = await fetch(`https://mern-social-media-website-git-main-sahilsagvekars-projects.vercel.app/posts`, {
+    const response = await fetch(`https://social-media-website-5.onrender.com/posts`, {
       method: "POST",
       headers: { Authorization: `Bearer ${token}` },
       body: formData,
