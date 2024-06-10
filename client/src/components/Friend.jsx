e859e6f079c7a70f25a139e2f35897b853cf04ce
Sahil Sagvekar <sahilsagvekar230@gmail.com>
2024-06-10 14:23:11 +0530

    Update Friend.jsx

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -23,7 +23,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 
   const patchFriend = async () => {
     const response = await fetch(
-      `https://social-media-website-ed9p.onrender.com/users/${_id}/${friendId}`,
+      `https://social-media-website-5.onrender.com/users/${_id}/${friendId}`,
       {
         method: "PATCH",
         headers: {
