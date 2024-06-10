52ce7342a7d7e45f680348b23d6183a7e46a6bf2
Sahil Sagvekar <sahilsagvekar230@gmail.com>
2024-06-10 14:25:13 +0530

    Update index.jsx

--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -15,7 +15,7 @@ const ProfilePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
 
   const getUser = async () => {
-    const response = await fetch(`https://social-media-website-ed9p.onrender.com/users/${userId}`, {
+    const response = await fetch(`https://social-media-website-5.onrender.com/users/${userId}`, {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
