8d667014e1ade006967450fe9b7c3174d8a0da48
Sahil Sagvekar <sahilsagvekar230@gmail.com>
2024-06-10 14:34:44 +0530

    Update PostsWidget.jsx

--- client/src/scenes/widgets/PostsWidget.jsx
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -9,7 +9,7 @@ const PostsWidget = ({ userId, isProfile = false }) => {
   const token = useSelector((state) => state.token);
 
   const getPosts = async () => {
-    const response = await fetch("https://mern-social-media-website-git-main-sahilsagvekars-projects.vercel.app/posts", {
+    const response = await fetch("https://social-media-website-5.onrender.com/posts", {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
@@ -19,7 +19,7 @@ const PostsWidget = ({ userId, isProfile = false }) => {
 
   const getUserPosts = async () => {
     const response = await fetch(
-      `https://mern-social-media-website-git-main-sahilsagvekars-projects.vercel.app/${userId}/posts`,
+      `https://social-media-website-5.onrender.com/${userId}/posts`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
